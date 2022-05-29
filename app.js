d4c64c7f6cf35252255064070520fc10c498bbf8
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-05-29 12:12:00 +0530

    complete html css and js

--- app.js
+++ app.js
@@ -1,6 +1,50 @@
-let data;
+// let data;
+// fetch("data.json")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     createAndRenderBars(data);
+//   });
+
+// let highestAmount = 0;
+// const chart = document.querySelector(".chart");
+
+// function createAndRenderBars(data) {
+//   getHighestValue(data);
+//   data.forEach((dataEl) => {
+//     const html = `
+//     <div class="bars">
+//         <div class="bar"></div>
+//         <p class="day" data-height = "${dataEl.amount}">${dataEl.day}</p>
+//     </div>
+//     `;
+//     chart.insertAdjacentHTML("beforeend", html);
+//   });
+// }
+
+// function getHighestValue(data) {
+//   console.log(data);
+//   data.forEach((dataEl) => {
+//     if (dataEl.amount > highestAmount) highestAmount = dataEl.amount;
+//     return highestAmount;
+//   });
+// }
+
+const bars = document.querySelectorAll(".bar");
+
+const amountHover = document.createElement("div");
+amountHover.className = "amount-hover";
+
 fetch("data.json")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
+    for (let i = 0; i < bars.length; i++) {
+      bars[i].addEventListener("mouseover", function () {
+        amountHover.innerHTML = `$${data[i].amount}`;
+        bars[i].appendChild(amountHover);
+      });
+
+      bars[i].addEventListener("mouseout", function () {
+        bars[i].removeChild(amountHover);
+      });
+    }
   });
